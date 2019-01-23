70d4db18bfe4a3f8ffb708a1053c9913fdf5dbd7
Robbie Redfearn <rredfearn32@live.com>
2019-01-23 14:39:34 +0000

    More work

--- js/script.js
+++ js/script.js
@@ -12,9 +12,17 @@ function positionShit() {
         // Add watchers for sprites
         bgSection.querySelectorAll('.sprite').forEach(sprite => {
             if (sprite.dataset.direction === 'down') {
-                sprite.style.backgroundPositionY = -((bgSection.getBoundingClientRect().top / sprite.dataset.speed) - sprite.dataset.offsety) + 'px';
+                sprite.style.backgroundPositionY = -((bgSection.getBoundingClientRect().top / sprite.dataset.speed) - sprite.dataset.offsety) + (sprite.dataset.offsety.indexOf('px') > -1 || sprite.dataset.offsety.indexOf('%') > -1 ? '' : 'px');
             } else if(sprite.dataset.direction === 'up') {
-                sprite.style.backgroundPositionY = (parseInt(sprite.dataset.offsety) + (parseInt(bgSection.getBoundingClientRect().top) * sprite.dataset.speed)) + 'px';
+                sprite.style.backgroundPositionY = (parseInt(sprite.dataset.offsety) + (parseInt(bgSection.getBoundingClientRect().top) * sprite.dataset.speed)) + (sprite.dataset.offsety.indexOf('px') > -1 || sprite.dataset.offsety.indexOf('%') > -1 ? '' : 'px');
+            }
+
+            if(sprite.dataset.scale) {
+                sprite.style.backgroundSize = parseInt(sprite.dataset.scale) - parseInt(bgSection.getBoundingClientRect().top) + (sprite.dataset.offsety.indexOf('px') > -1 || sprite.dataset.offsety.indexOf('%') > -1 ? '' : 'px');
+            }
+
+            if(sprite.dataset.offsetx) {
+                sprite.style.backgroundPositionX = sprite.dataset.offsetx;
             }
         });
     });
